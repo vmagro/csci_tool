d265864d02215709d1e1d8625d3618a830ec5ffc
Vinnie Magro <v@vinnie.io>
2017-09-09 13:07:26 -0700

    Merge grades with blackboard csv

--- csci_tool/commands/__init__.py
+++ csci_tool/commands/__init__.py
@@ -2,6 +2,7 @@ from .collect import CollectCommand
 from .collect_late import CollectLateCommand
 from .create_repos import CreateReposCommand
 from .login import LoginCommand
+from .merge_blackboard import MergeBlackboardCommand
 from .mutate import MutateCommand
 from .grade import GradeCommand
 
@@ -10,6 +11,7 @@ subcommands = [
     CollectLateCommand(),
     CreateReposCommand(),
     LoginCommand(),
+    MergeBlackboardCommand(),
     MutateCommand(),
     GradeCommand(),
 ]
