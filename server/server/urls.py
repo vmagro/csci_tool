4c8448734a5293229f7378b295fe78bd89bfd2f4
Vinnie Magro <v@vinnie.io>
2017-09-21 13:11:12 -0700

    Change url prefix to gitbot/

--- server/server/urls.py
+++ server/server/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^', include('gitbot.urls')),
+    url(r'^gitbot/', include('gitbot.urls')),
     url(r'^admin/', admin.site.urls),
 ]
