fc833058b5b65e9b6f9f9e43e0699f61968c2c62
Vinnie Magro <v@vinnie.io>
2017-09-21 02:56:24 -0700

    Add more generic methods to LocalRepo to be useful for assignments repo

--- server/gitbot/repo.py
+++ server/gitbot/repo.py
@@ -7,7 +7,7 @@ from tempfile import TemporaryDirectory
 from typing import Type
 
 from .app_settings import BOT_PRIVATE_KEY_PATH as private_key_path
-from .models import Student, Repo as DBRepo
+from .models import Repo as DBRepo
 
 
 logger = logging.getLogger(__name__)
@@ -17,18 +17,24 @@ class LocalRepo(object):
     """A LocalRepo is a copy of a student repo stored on disk."""
 
     path: str
-    student: Type[Student]
+
+    def __init__(self, path: str):
+        """Init a LocalRepo at a specific path."""
+        self.path = path
+
+    @staticmethod
+    def clone_from_url(url: str, path: str):
+        """Clone a repo to local disk given a git url."""
+        logger.debugh('Repo path: %s', path)
+        instance = LocalRepo(path)
+        subprocess.run(['git', 'clone', '--depth', '1', url, instance.path])
 
     @staticmethod
     def clone_repo(repo_stub: Type[DBRepo]):
         """Clone a repo to local disk given information from the database."""
         logger.info('Cloning repo for %s', repo_stub.student.unix_name)
-        instance = LocalRepo()
-        path = TemporaryDirectory(prefix='gitbot', suffix=repo_stub.student.unix_name)
-        logger.debugh('Repo path: %s', path)
-        instance.path = path
-        subprocess.run(['git', 'clone', '--depth', '1', repo_stub.repo_url, instance.path])
-        return instance
+        path = TemporaryDirectory(prefix='gitbot')
+        return LocalRepo.clone_from_url(repo_stub.repo_url, path)
 
     def run(self, cmd, **kwargs):
         """Run the command list through subprocess in the repo working directory."""
@@ -54,6 +60,10 @@ class LocalRepo(object):
     def push(self):
         """Push local commit(s) to GitHub."""
         self.run(['git', 'push'])
+
+    def pull(self):
+        """Pull remote changes from GitHub."""
+        self.run(['git', 'pull'])
 
     def delete(self):
         """Delete the temporary directory."""
