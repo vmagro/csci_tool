ee2a31512ba033161eab7d9987519db578cda6af
Vinnie Magro <v@vinnie.io>
2017-09-21 01:38:47 -0700

    Collection task that saves a Submission object to the db

--- server/gitbot/tasks.py
+++ server/gitbot/tasks.py
@@ -3,7 +3,7 @@ from typing import Type, List
 from celery import shared_task, chain, group
 from celery.utils.log import get_task_logger
 
-from .models import Student, Assignment
+from .models import Student, Assignment, Submission
 from .repo import LocalRepo
 
 logger = get_task_logger(__name__)
@@ -60,7 +60,25 @@ def give_assignment(student: Type[Student], assignment: Type[Assignment]):
 
 @shared_task
 def give_assignment_to_all(students: List[Student], assignment: Type[Assignment]):
-    """Update all student's repos using the mutation from an assignment."""
+    """Update a list of students's repos using the mutation from an assignment."""
     give_tasks = [give_assignment.s(student, assignment) for student in students]
     g = group(give_tasks)
+    return g.apply_async()
+
+
+@shared_task
+def collect_assignment(student: Type[Student], assignment: Type[Assignment]):
+    """Save a Submission object tying a Student Commit to the given Assignment."""
+    commit = student.repo.head_commit()
+    logger.info('Latest commit for %s: %s', student.unix_name, commit.sha)
+    submission = Submission(student=student, assignment=assignment, commit=commit)
+    submission.save()
+    # TODO comment on the commit so the student knows we collected it
+
+
+@shared_task
+def collect_assignment_from_all(students: List[Student], assignment: Type[Assignment]):
+    """Collect an Assignment from a list of students."""
+    collect_tasks = [collect_assignment.s(student, assignment) for student in students]
+    g = group(collect_tasks)
     return g.apply_async()
