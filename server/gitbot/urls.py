f95a1222fadf69a2d20a44a0f0741c28e25c12be
Vinnie Magro <v@vinnie.io>
2017-09-21 13:11:37 -0700

    Enable coreapi schema

--- server/gitbot/urls.py
+++ server/gitbot/urls.py
@@ -2,13 +2,18 @@
 from django.conf.urls import url, include
 from rest_framework.routers import DefaultRouter
 
+from rest_framework.schemas import get_schema_view
+
 from . import views
 from . import hooks
 
 router = DefaultRouter(trailing_slash=False)
 router.register(r'students', views.StudentViewSet)
 
+schema_view = get_schema_view(title='Gitbot API')
+
 urlpatterns = [
     url(r'^', include(router.urls)),
-    url(r'^hook/push$', hooks.push_hook)
+    url(r'^hook/push$', hooks.push_hook),
+    url(r'^schema/$', schema_view),
 ]
