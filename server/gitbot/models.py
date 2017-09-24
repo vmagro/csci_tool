59aa9ebd847a974af40925c4618ba1884ebddcc6
Vinnie Magro <v@vinnie.io>
2017-09-24 10:51:20 -0700

    Add grades field to Submission model

--- server/gitbot/models.py
+++ server/gitbot/models.py
@@ -1,6 +1,7 @@
 """Database models supporting the gitbot application."""
 
 from django.db import models
+from django.core.validators import validate_comma_separated_integer_list
 
 from .app_settings import GITHUB_ORG
 
@@ -86,6 +87,10 @@ class Submission(models.Model):
     commit = models.ForeignKey(Commit, on_delete=models.CASCADE)
     date = models.DateTimeField(auto_now_add=True)
     late_days_used = models.IntegerField(default=0)
+
+    # store a "list" of grades as comma separated integers
+    grades = models.CharField(max_length=100, default='',
+                              validators=[validate_comma_separated_integer_list])
 
     class Meta:
         unique_together = ('student', 'assignment', 'commit')
