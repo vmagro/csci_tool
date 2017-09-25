e58e053a56bec7a95e55a6123d8417e7d2485626
Vinnie Magro <v@vinnie.io>
2017-09-24 17:25:57 -0700

    Register Submission and Mutation in admin site

--- server/gitbot/admin.py
+++ server/gitbot/admin.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Student, Repo, Commit, Assignment
+from .models import Student, Repo, Commit, Assignment, Submission, Mutation
 
 admin.site.register(Student)
 admin.site.register(Repo)
 admin.site.register(Commit)
 admin.site.register(Assignment)
+admin.site.register(Submission)
+admin.site.register(Mutation)
