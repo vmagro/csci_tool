c5d5bbaa0dbc5589f43f2e76f9d6fee52e4abef1
Vinnie Magro <v@vinnie.io>
2017-09-24 17:54:59 -0700

    Add Assignment model to cli

--- cli/csci_tool/models.py
+++ cli/csci_tool/models.py
@@ -8,6 +8,16 @@ class Student(object):
         self.last_name = d['last_name']
         self.github_username = d['github_username']
 
-
     def __str__(self):
         return f'{self.preferred_name} {self.last_name} <{self.usc_email}>'
+
+
+class Assignment(object):
+
+    def __init__(self, d: dict):
+        self.path = d['path']
+        self.due_date = d['due_date']
+        self.status = d['status']
+
+    def __str__(self):
+        return f'{self.path} due at {self.due_date}: "{self.status}"'
