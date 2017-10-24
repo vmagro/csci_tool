5d16664d0b266111efaa78694507ef37c37855df
Vinnie Magro <v@vinnie.io>
2017-10-24 12:50:13 -0700

    Delete server/

--- server/gitbot/tasks.py
+++
@@ -1,6 +0,0 @@
-"""Celery tasks for gitbot."""
-
-# import all the other tasks files so that celery can find them
-from .collect_tasks import * # noqa
-from .deploy_tasks import * # noqa
-from .meta_tasks import * # noqa
