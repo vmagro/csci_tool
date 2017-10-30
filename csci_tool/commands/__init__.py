e498ab154ed2e47a0f3ac5506ee21cf4ea274b0b
Vinnie Magro <v@vinnie.io>
2017-10-29 18:09:27 -0700

    golang .gitignore

--- csci_tool/commands/__init__.py
+++
@@ -1,9 +0,0 @@
-from .course import course
-from .students import student
-from .assignments import assignment
-
-subcommands = [
-    course,
-    student,
-    assignment,
-]
--- csci_tool/tasks/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Celery tasks for gitbot."""
-
-# import all the other tasks files so that celery can find them
-from .collect_tasks import * # noqa
-from .deploy_tasks import * # noqa
