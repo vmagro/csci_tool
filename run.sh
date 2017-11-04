5042b45c3b8295ff6800948eb88b8eff8a15a045
vmagrobot <v+gitbot@vinnie.io>
2017-11-03 23:11:46 -0700

    Run for all students

--- run.sh
+++ run.sh
@@ -8,13 +8,17 @@ source clone.sh
 
 function run_for_student {
   unixname=$1
-  run=${@:2}
+  studentid=$2
+  run=${@:3}
 
   path="$(clone_student $unixname)"
 
   # set a variable to reference the assignments dir where the script is
   # this script must be run from within the assignments directory
   export ASSIGNMENTS_DIR="$(pwd)"
+  # export some data about the student that the subprocess can use
+  export STUDENT=$unixname
+  export STUDENT_ID=$studentid
   # make sure we're in the student directory
   pushd $path > /dev/null
   # run whatever we were supposed to run
@@ -30,5 +34,11 @@ function run_for_student {
 # bash says the source file name is
 if [[ "$0" = "$BASH_SOURCE" ]];
 then
-  run_for_student $1 ${@:2}
+  students=$(load_students "$1")
+  while read -r student; do
+    # we can abuse string "expansion" to pass $student as multiple variables to
+    # the run_for_student function
+    echo "Running on $student"
+    run_for_student $student ${@:2}
+  done <<< "$students"
 fi
