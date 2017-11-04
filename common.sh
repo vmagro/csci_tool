6f65e56b9b004ee981a89f229ee0c181f48f1778
vmagrobot <v+gitbot@vinnie.io>
2017-11-03 23:11:37 -0700

    Load students from csv

--- common.sh
+++ common.sh
@@ -1,2 +1,9 @@
+set -e
+
 source config.sh
 
+function load_students {
+  path=$1
+  # unix name and usc id are the first two columns separated by a comma
+  awk -F',' '{print $1 " " $2}' "$path"
+}
